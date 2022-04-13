27f369f6c5478965979cba7d478abc1a46935fc1
Jennika Wilson <j.r.a.112100@gmail.com>
2022-04-13 18:46:40 -0400

    restarting

--- script.js
+++
@@ -1,8 +0,0 @@
-// calling on snake img
-let snake = document.getElementById('id')
-
-//snake move function
-snake.addEventListener('keydown', function(e){
-    let x = 0 + "px"
-    let y = 0 + "px"
-})
