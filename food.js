96c09237b233859ea5792b78cceb5f4141f6b3fc
Jennika Wilson <j.r.a.112100@gmail.com>
2022-04-14 17:05:49 -0400

    Finished food

--- food.js
+++ food.js
@@ -1,22 +1,27 @@
-//imp from other js
-import {onSnake, expandSnake} from './snake.js'
+import { onSnake, expandSnake } from './snake.js'
 
-//dec var
-let food = {x: 10, y: 3}
-const expantionRate = 1
+let food = getRandomFoodPosition()
+const EXPANSION_RATE = 5
 
-//food function
-export function update(){
-    if (onSnake(food)) {
-        expandSnake(expantionRate)
-        food = {x: 20, y: 3}
-    }
+export function update() {
+  if (onSnake(food)) {
+    expandSnake(EXPANSION_RATE)
+    food = getRandomFoodPosition()
+  }
 }
 
 export function draw(gameBoard) {
-       const foodElement = document.createElement('div')
-       foodElement.style.gridRowStart = food.y
-       foodElement.style.gridColumnStart = food.x
-       foodElement.classList.add('food')
-       gameBoard.appendChild(foodElement)
+  const foodElement = document.createElement('div')
+  foodElement.style.gridRowStart = food.y
+  foodElement.style.gridColumnStart = food.x
+  foodElement.classList.add('food')
+  gameBoard.appendChild(foodElement)
+}
+
+function getRandomFoodPosition() {
+  let newFoodPosition
+  while (newFoodPosition == null || onSnake(newFoodPosition)) {
+    newFoodPosition = randomGridPosition()
+  }
+  return newFoodPosition
 }
